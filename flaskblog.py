2cb1f5ebb6b10a9108d55384f84bb856688e2231
Raju Thomas <raju.thomas@suvotechnology.com>
2021-12-16 11:18:23 +0000

    First commit

---
+++ flaskblog.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return "Home page"
+
+
+@app.route("/about")
+def about():
+    return "About page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
