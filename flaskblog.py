3b5c9248b091356ef535cc323948eee1216c12a5
Raju Thomas <raju.thomas@suvotechnology.com>
2021-12-27 09:25:39 +0000

    Jinja template engine

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        "title": "Title 1",
+        "author": "Autho 1",
+        "date": "12-12-2021",
+        "content": "Content 1"
+    },
+    {
+        "title": "Title 2",
+        "author": "Author 2",
+        "date": "12-12-2021",
+        "content": "Content 2"
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "Home page"
+    return render_template("home.html")
 
 
 @app.route("/about")
 def about():
-    return "About page"
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", posts=posts)
 
 
 if __name__ == "__main__":
